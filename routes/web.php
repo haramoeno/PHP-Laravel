c03e83b2ff9dcd201e8b7752112963ad413677e4
haramoeno <moe.piece1201@ezweb.ne.jp>
2024-08-23 11:16:47 +0000

    課題

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
     
 Route::controller(AAAController::class)->group(function() {
@@ -29,9 +29,12 @@ Route::controller(AAAController::class)->group(function() {
 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add');
+    Route::get('profile/create', 'add')->middleware('auth');
 });
     
 Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/edit', 'edit');
+    Route::get('profile/edit', 'edit')->middleware('auth');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
