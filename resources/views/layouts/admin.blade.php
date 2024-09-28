9b22df90be3bd9e7718b629883888fdf6aa6ae11
haramoeno <moe.piece1201@ezweb.ne.jp>
2024-09-28 13:32:04 +0000

    0926誤字修正

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
         
@@ -63,7 +63,7 @@
                                    {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
                                 
-                                <div class="dropdown-menu" aria-labelledby="navberDropdown">
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
--- webpack.mix.js
+++ webpack.mix.js
@@ -17,4 +17,4 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/profile.scss', 'public/css')
     .sass('resources/sass/front.css', 'public/css')
     
-    .sourceMaps()
+    .sourceMaps();
